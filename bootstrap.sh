c9a2ab1abbdc7522b196a793bcc3b8c16ddec024
Harry Brundage <harry.brundage@gmail.com>
2011-04-05 11:14:36 -0400

    Added bootstrap script to make all the links

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+ln -s bash/.bash_profile ../.bash_profile
+ln -s bash/.bashrc ../.bashrc
+ln -s git/.gitignore ../.gitignore
+ln -s git/.gitconfig ../.gitconfig
+ln -s vim/.gvimrc ../.gvimrc
+ln -s vim/.vimrc ../.vimrc
+ln -s vim/ ../.vim
