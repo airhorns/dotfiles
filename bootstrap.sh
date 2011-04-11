2cc5c36553fb13b12c9edf7b35ebf691bcd49942
Harry Brundage <harry.brundage@gmail.com>
2011-04-11 19:55:43 -0400

    Added screen

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
-rm ../.bash_profile ../.bashrc ../.gitignore ../.gitconfig ../.gvimrc ../.vimrc ../.vim
+rm ../.bash_profile ../.bashrc ../.gitignore ../.gitconfig ../.gvimrc ../.vimrc ../.vim ../.toprc ../.screenrc
 
-ln -s ~/dotfiles/bash/.bash_profile ../.bash_profile
-ln -s ~/dotfiles/bash/.bashrc ../.bashrc
-ln -s ~/dotfiles/git/.gitignore ../.gitignore
-ln -s ~/dotfiles/.gitconfig ../.gitconfig
-ln -s ~/dotfiles/vim/.gvimrc ../.gvimrc
-ln -s ~/dotfiles/vim/.vimrc ../.vimrc
+ln -s ~/dotfiles/bash/bash_profile ../.bash_profile
+ln -s ~/dotfiles/bash/bashrc ../.bashrc
+ln -s ~/dotfiles/git/gitignore ../.gitignore
+ln -s ~/dotfiles/git/gitconfig ../.gitconfig
+ln -s ~/dotfiles/vim/gvimrc ../.gvimrc
+ln -s ~/dotfiles/vim/vimrc ../.vimrc
 ln -s ~/dotfiles/vim/ ../.vim
+ln -s ~/dotfiles/screenrc ../.screenrc
+ln -s ~/dotfiles/toprc ../.toprc
