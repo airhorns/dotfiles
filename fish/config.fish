d27568d8b90d0d1684ca9cd0786cab2a83093ec4
Harry Brundage <harry.brundage@gmail.com>
2012-06-07 16:41:36 -0400

    More fish stuff for autojump and rbenv.

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,20 @@
-set PATH $PATH "$HOME/bin" "/usr/local/bin" "/usr/local/sbin" "./node_modules/.bin" "/usr/local/share/npm/bin"
-set EDITOR "vim"
-set BUNDLE_EDITOR "vim"
-set NODE_PATH "/usr/local/lib/node" $NODE_PATH
+set -xg PATH "$HOME/bin" "/usr/local/bin" "/usr/local/sbin" $PATH
+set -xg EDITOR "vim"
+set -xg BUNDLE_EDITOR "vim"
+set -xg NODE_PATH "/usr/local/lib/node" $NODE_PATH
 
-set PAGER "less"
+set -xg PAGER "less"
+
+alias vi vim
 
 set host_spec $HOME/dotfiles/bash/(hostname)_spec.sh
 set host_spec (echo $host_spec | sed s/\.local//)
 
-set fish_greeting "Hello."
+set fish_greeting ""
+
+# Add rbenv init stuff
+set -xg PATH "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" $PATH
+rbenv rehash 2>/dev/null
 
 if test -f "$host_spec"
   . host_spec
