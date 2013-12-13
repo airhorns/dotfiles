5a8c01eeb7269cffec3d65c9560c6a5e8e704f3f
Harry Brundage <harry.brundage@gmail.com>
2013-12-13 00:20:33 +0000

    Updates for vagrant bootstrap

---
+++ fish/shopify-dev_spec.fish
@@ -0,0 +1,9 @@
+set -xg RUBY_HEAP_MIN_SLOTS 800000
+set -xg RUBY_HEAP_FREE_MIN 100000
+set -xg RUBY_HEAP_SLOTS_INCREMENT 300000
+set -xg RUBY_HEAP_SLOTS_GROWTH_FACTOR 1
+set -xg RUBY_GC_MALLOC_LIMIT 79000000
+
+. ~/dotfiles/fish/hook/hook.fish
+fish_hook_enable autojump brew debugging ln rake rails git
+
