4bf0c09419b3fcd97a9dd2564b8d8510adb58363
Harry Brundage <harry.brundage@gmail.com>
2013-12-12 19:09:40 -0500

    Prompt with host in it

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,9 +1,12 @@
 function fish_prompt -d "Write out the prompt"
+  # Hostname
+  printf '%s%s' (set_color magenta) (hostname -s)
+
 	# Color writeable dirs green, read-only dirs red
 	if test -w "."
-		printf '%s%s' (set_color blue) (prompt_pwd)
+		printf ' %s%s' (set_color blue) (prompt_pwd)
 	else
-		printf '%s%s' (set_color red) (prompt_pwd)
+		printf ' %s%s' (set_color red) (prompt_pwd)
 	end
 
   # spit out virtual env
@@ -18,5 +21,5 @@ function fish_prompt -d "Write out the prompt"
 
 	# Print git branch
   printf '%s%s' (set_color green) (__git_ps1)
-	printf '%s ➜  %s' (set_color -o purple) (set_color normal)
+	printf '%s ➜  %s' (set_color -o brown) (set_color normal)
 end
