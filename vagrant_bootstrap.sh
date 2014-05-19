468b5c3362852b01bd95ba3797e50ae76c06c58e
Harry Brundage <harry.brundage@gmail.com>
2014-05-19 03:35:30 +0000

    Vagrant fixes for hook

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -3,4 +3,5 @@ sudo apt-get install python-software-properties
 sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get install fish ctags autojump vim-nox
+git clone https://github.com/airhorns/hook.git ~/dotfiles/fish/hook
 chsh -s /usr/bin/fish
