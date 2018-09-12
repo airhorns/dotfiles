8f83b57e48f93118a05dcac8583e2fc06ff8c4bd
Harry Brundage <harry.brundage@gmail.com>
2018-09-12 10:43:50 -0400

    Add Pilot spec

---
+++ fish/pilot_spec.fish
@@ -0,0 +1,12 @@
+# set -xg PATH $PATH
+
+# Virtualenv
+set -xg WORKON_HOME "/Users/airhorns/.virtualenvs"
+# eval (python -m virtualfish auto_activation)
+
+set --local openssl_prefix "/usr/local/opt/openssl"
+set -xg EJSON_KEYDIR "/Users/airhorns/.ejson/keys"
+set -xg CFLAGS "-I$openssl_prefix/include"
+set -xg LDFLAGS "-L$openssl_prefix/lib"
+
+#source ~/.config/fish/functions/fish_avn.fish
